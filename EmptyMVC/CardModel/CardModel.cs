ceac9ddc9ef8cb83bb493cfec5f3c0d6e71c0432
seva <seva1398@domain.com>
2021-06-14 22:44:45 +0300

    Add Card Model

---
+++ EmptyMVC/CardModel/CardModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RumExceptions;
+
+namespace MechanicsModel
+{
+    public class Card
+    {
+        private CardColor _color;
+        private int _number;
+
+        public Card(CardColor Color, int Number)
+        {
+            _color = Color;
+
+            if (Color is CardColor.Joker)
+            {
+                _number = 0;
+                return;
+            }
+
+            if (Number > 13)
+            {
+                throw new RumExceptionException(ExceptionType.CardError01, "Значение фишки не может быть больше 13");
+            }
+
+            if (Number < 1)
+            {
+                throw new RumExceptionException(ExceptionType.CardError01, "Значение фишки не может быть меньше 1");
+            }
+
+            _number = Number;
+        }
+    }
+
+    public enum CardColor
+    {
+        Red = 0,
+        Yellow,
+        Blue,
+        Black,
+        Joker
+    }
+}
---
+++ EmptyMVC/RumExceptions/RumExceptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumExceptions
+{
+    public class RumExceptionException : ApplicationException
+    {
+        private ExceptionType _exType;
+
+        public RumExceptionException(ExceptionType exceptionType, string message) : base(message)
+        {
+            _exType = exceptionType;
+        }
+    }
+
+
+    public enum ExceptionType
+    {
+        /// <summary>
+        /// ошибка при формировании фишки
+        /// </summary>
+        CardError01 = 0
+    }
+}
